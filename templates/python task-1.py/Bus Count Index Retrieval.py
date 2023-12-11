9641ee3a97ef8ccc92e2410e6eee6d77d2355806
Dhanya N <104639179+123dhanya@users.noreply.github.com>
2023-12-11 15:34:20 +0530

    Create Bus Count Index Retrieval.py

---
+++ templates/python task-1.py/Bus Count Index Retrieval.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def filter_routes(df):
+    # Group by 'route' and calculate the average of the 'truck' column
+    route_avg_truck = df.groupby('route')['truck'].mean()
+
+    # Filter routes where the average of 'truck' values is greater than 7
+    selected_routes = route_avg_truck[route_avg_truck > 7].index.tolist()
+
+    # Sort the list of selected routes
+    selected_routes.sort()
+
+    return selected_routes
+
+# Example usage
+dataset_path = 'dataset-1.csv'
+df = pd.read_csv(dataset_path)
+result_list = filter_routes(df)
+print(result_list)
